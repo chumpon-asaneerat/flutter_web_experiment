16663deea103af81c548aff585ace272cb074edb
chumpon-asaneerat <chumponsenate@gmail.com>
2024-03-02 09:21:08 +0700

    offline for build experiment #1

---
+++ build.cmd
@@ -0,0 +1,9 @@
+REM EXAMPLES
+REM flutter build web --base-href "/subpath/" --no-web-resources-cdn
+
+REM BUILD MODE
+flutter build web --base-href "/" --no-web-resources-cdn
+
+REM RELEASE MODE
+REM flutter build web --release --base-href "/" --no-web-resources-cdn
+
